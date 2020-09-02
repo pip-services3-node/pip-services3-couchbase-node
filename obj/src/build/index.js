f4da88a6afa5767b12019e2b2729f011728bb8ec
EgorNuzhnykh <nuzhnykhev@gmail.com>
2020-09-02 18:19:49 +0000

    project build by Travis CI [skip ci]

--- obj/src/build/index.js
+++ obj/src/build/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.DefaultCouchbaseFactory = void 0;
 /**
  * @module build
  * @preferred
--- obj/src/connect/index.js
+++ obj/src/connect/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CouchbaseConnectionResolver = exports.CouchbaseConnectionParams = void 0;
 /**
  * @module connect
  * @preferred
