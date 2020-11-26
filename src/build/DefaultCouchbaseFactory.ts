ae6a15eafc41b051bb7b6a8866fe642d7403b4ad
banalna <judas.priest999@gmail.com>
2020-11-26 13:30:33 +0300

    fix links in docs

--- src/build/DefaultCouchbaseFactory.ts
+++ src/build/DefaultCouchbaseFactory.ts
@@ -7,7 +7,7 @@ import { CouchbaseConnection } from '../persistence/CouchbaseConnection';
 /**
  * Creates Couchbase components by their descriptors.
  * 
- * @see [[https://rawgit.com/pip-services-node/pip-services3-components-node/master/doc/api/classes/build.factory.html Factory]]
+ * @see [[https://pip-services3-node.github.io/pip-services3-components-node/classes/build.factory.html Factory]]
  * @see [[CouchbaseConnection]]
  */
 export class DefaultCouchbaseFactory extends Factory {
